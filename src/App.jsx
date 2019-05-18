d5c6d2a5753108b5830175401864f09cc381377f
k3klein <47892995+k3klein@users.noreply.github.com>
2019-05-18 19:55:17 +0200

    functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import FileSaver from 'file-saver';
 import 'uikit/dist/css/uikit.css';
-import 'uikit/dist/js/uikit';
+import UIkit from 'uikit';
 import './App.css';
+import Icons from 'uikit/dist/js/uikit-icons';
+import 'uikit/dist/js/uikit.js';
+
+UIkit.use(Icons);
 
 function Task({ index, task, removeTask, setCompleted }) {
   //const [completed, setCompleted] = useState(false);
   return (
-    <div className={"uk-container uk-animation-fade"}>
+    <div className={"uk-container uk-animation-fade uk-margin-top"}>
       {/*console.log(task.text)*/}
-      {task.completed ? <s>{task.text}</s> : task.text}
-      <button className={"uk-button uk-button-default uk-margin-left uk-margin-right uk-align-right"} onClick={() => setCompleted(index)}>Completed</button>
-      <button className={"uk-button uk-button-danger uk-align-right"} onClick={() => removeTask(index)}>X</button>
+      <span className={"uk-text-lead uk-text-middle uk-text-break"}>{task.completed ? <s>{task.text}</s> : task.text}</span>
+      <div className={"uk-align-right"}>
+        <button className={"uk-button uk-button-default uk-margin-left uk-margin-right"} onClick={() => setCompleted(index)}>Completed</button>
+        <button className={"uk-button uk-button-danger"} onClick={() => removeTask(index)}>X</button>
+      </div>
+      <hr className={"uk-divider-small"} />
     </div>
   );
 }
@@ -26,7 +33,7 @@ function Input({ addTask }) {
   return (
     <div>
       <input className={"uk-input"} placeholder={"Write something here."} type="text" onChange={handleChange} />
-      <button className={"uk-button uk-button-default"} onClick={() => { text !== "" ? addTask(text) : alert("Enter Text to add.") }}>Add Task</button>
+      <button className={"uk-button uk-button-primary uk-margin-top"} onClick={() => { text !== "" ? addTask(text) : UIkit.notification('Please type something.', 'danger') }}>Add Task</button>
     </div>
   );
 }
@@ -35,10 +42,7 @@ function Input({ addTask }) {
 function App() {
   const initialTasks = () => JSON.parse(window.localStorage.getItem("tasks")) ||
     [
-      { text: "Task Number 1", completed: false },
-      { text: "Task Number 2", completed: false },
-      { text: "Task Number 3", completed: false },
-      { text: "Task Number 4", completed: false },
+      { text: "Hi, this is a Task!", completed: false }
     ];
 
   //console.log(JSON.parse(window.localStorage.getItem("tasks")));
@@ -55,16 +59,25 @@ function App() {
   }
 
   const setAllCompleted = () => {
-    let newTasks = [...tasks];
-    for (let index = 0; index < newTasks.length; index++) {
-      const element = newTasks[index];
-      element.completed = true;
+    if (tasks.length === 0) {
+      UIkit.notification('No tasks.', 'danger');
+    } else {
+      let newTasks = [...tasks];
+      for (let index = 0; index < newTasks.length; index++) {
+        const element = newTasks[index];
+        element.completed = true;
+      }
+      setTasks(newTasks);
     }
-    setTasks(newTasks);
   }
 
   const removeAllTasks = () => {
-    setTasks([]);
+    UIkit.modal.confirm('Are you sure?').then(function () {
+      console.log('Confirmed.')
+      setTasks([]);
+    }, function () {
+      console.log('Rejected.')
+    });
   }
 
   useEffect(() => {
@@ -81,6 +94,7 @@ function App() {
     let newTasks = [...tasks];
     newTasks.splice(index, 1);
     setTasks(newTasks);
+
   }
 
   const saveTasks = () => {
@@ -92,24 +106,30 @@ function App() {
       }
       console.log(text);
       var blob = new Blob([text], { type: "text/plain;charset=utf-8" });
-      FileSaver.saveAs(blob, "hello world.txt");
+      FileSaver.saveAs(blob, "tasks.txt");
     } else {
-      window.alert("At least add one task.");
+      UIkit.notification('At least add one task.', 'danger');
     }
 
   }
 
 
   return (
-    <div className={"uk-container uk-animation-fade"}>
-      <p><h1>Remaining Tasks: {tasks.filter(task => task.completed === false).length}</h1></p>
-      <p><h3>Total Tasks: {tasks.length}</h3></p>
-      {tasks.map((task, index) => <Task key={index} index={index} task={task} removeTask={removeTask} setCompleted={setCompleted} />)}
-      <Input addTask={addTask} />
-      <button className={"uk-button uk-button-default"} onClick={setAllCompleted}>Set all to Completed</button>
-      <button className={"uk-button uk-button-default"} onClick={removeAllTasks}>Remove all Tasks</button>
-      <button className={"uk-button uk-button-default"} onClick={saveTasks}>Save all tasks as txt</button>
-    </div>
+    <React.Fragment>
+      <h1 className={"uk-background-primary uk-padding-small uk-animation-fade uk-text-bold"}>Neat Task Manager</h1>
+      <div className={"uk-container uk-animation-fade uk-margin-large-top"}>
+        <h1 className={"uk-text-primary"}><p>Remaining Tasks: {tasks.filter(task => task.completed === false).length}</p></h1>
+        <h5><p>Total Tasks: <span className="uk-badge">{tasks.length}</span></p></h5>
+        {tasks.map((task, index) => <Task key={index} index={index} task={task} removeTask={removeTask} setCompleted={setCompleted} />)}
+        <hr className="uk-divider-icon"></hr>
+        <Input addTask={addTask} />
+        <button className={"uk-button uk-button-default uk-margin-top uk-margin-right"} onClick={setAllCompleted}>Set all to Completed</button>
+        <button className={"uk-button uk-button-default uk-margin-top uk-margin-right"} onClick={removeAllTasks}>Remove all Tasks</button>
+        <button className={"uk-button uk-button-default uk-margin-top uk-margin-right"} onClick={saveTasks}>
+          <span uk-icon="download"></span> Save all tasks as txt
+      </button>
+      </div >
+    </React.Fragment>
   );
 }
 
